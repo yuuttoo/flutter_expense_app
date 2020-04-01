049ca509352b89a317d2fb453165170d37b042e9
Chung Cheng Yu <50949562+yuuttoo@users.noreply.github.com>
2020-04-01 17:22:58 +0800

    change theme color

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -23,7 +23,7 @@ class TransactionList extends StatelessWidget{
                      ),
                    decoration: BoxDecoration(
                      border: Border.all(
-                       color: Colors.purple, 
+                       color: Theme.of(context).primaryColor,
                        width: 2,
                        ),
                       ),  
@@ -32,7 +32,7 @@ class TransactionList extends StatelessWidget{
                      '\$${transactions[index].amount.toStringAsFixed(2)}',
                      style: TextStyle(fontWeight: FontWeight.bold,
                      fontSize: 20,
-                     color: Colors.purple,
+                     color: Theme.of(context).primaryColor,
                      ),
                     ),
                   ),
